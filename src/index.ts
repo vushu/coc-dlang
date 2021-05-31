9d5495154129b6ad25838517a3d46f6e8eac0ebe
Dan Hoang Vu <vushu@pop-os.localdomain>
2021-05-31 09:46:02 +0200

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-import { workspace, ExtensionContext, services, LanguageClient } from 'coc.nvim';
+import { TextEdit, window, workspace, ExtensionContext, services, LanguageClient, commands, TextDocument, Position, Thenable,TextDocumentIdentifier } from 'coc.nvim';
+//import { Range, window, workspace, ExtensionContext, services, LanguageClient, commands, TextDocument, Position, Thenable } from 'vs-code-language-server-protocol';
+interface ImplementMethodsParams
+{
+  /** Text document to look in */
+  textDocument: TextDocumentIdentifier;
+
+  /** Location of cursor as standard offset */
+  location: number;
+}
+
+interface CodeReplacement
+{
+  /**
+   * Range what to replace. If both indices are the same its inserting.
+   *
+   * This value is specified as bytes offset from the UTF-8 source.
+   */
+  range: number;
+
+  /** Content to replace it with. Empty means remove. */
+  content: string;
+}
+
+interface ImportModification
+{
+  /** Set if there was already an import which was renamed. (for example import io = std.stdio; would be "io") */
+  rename: string;
+
+  /** Array of replacements to add the import to the code */
+  replacements: CodeReplacement[];
+}
 
 export async function activate(context: ExtensionContext): Promise<void> {
   const config = workspace.getConfiguration('coc-dlang');
@@ -6,17 +37,142 @@ export async function activate(context: ExtensionContext): Promise<void> {
   if (!isEnable) {
     return;
   }
+
   const serverOptions = {
     command: 'serve-d', // run serve-d
+    args: [
+      '--require', 'D',
+      '--lang', 'en',
+      '--provide', 'http',
+      '--provide', 'implement-snippets',
+      '--provide', 'context-snippets'],
   };
   const clientOptions = {
     documentSelector: ['d'], // run serve-d on d files
   };
   const client = new LanguageClient(
-    'coc-dlang', // the id
+    'dlang', // the id
     'coc-dlang', // the name of the language server
     serverOptions,
     clientOptions
   );
-  context.subscriptions.push(services.registLanguageClient(client));
+
+
+  workspace.registerKeymap(['n', 'i'], 'dlang-dostuff', async () => {
+    await commands.executeCommand('dlang.dostuff');
+  });
+
+  const commandIgnoreDscannerKey = commands.registerCommand('code-d.ignoreDscannerKey', async () => {
+    window.showPrompt("hello");
+  });
+
+
+  const commandlistArchTypes = commands.registerCommand("dlang.listArchTypes", async () => {
+    client.sendRequest<string[]>('served/listArchTypes').then((result) => {
+      window.showInformationMessage("serve-d archtypes: " + result.join(","));
+    });
+  });
+
+  const commandListConfiguration = commands.registerCommand("dlang.listConfigurations", async () => {
+    client.sendRequest<string[]>('served/listConfigurations').then((result) => {
+      window.showInformationMessage("listConfiguration: " + result.join(","));
+    });
+  });
+
+  const commandImplementMethods = commands.registerCommand("dlang.implementMethods", async () => {
+    const document = await workspace.document
+    const position = await window.getCursorPosition();
+    const range = document.getWordRangeAtPosition(position);
+    const cursorPos = await window.getCursorScreenPosition();
+    const location = document.textDocument.offsetAt(position)
+    const location2 = document.getPosition(cursorPos.row, cursorPos.col);
+    let currentWord = document.textDocument.getText(range);
+    let uri = document.uri;
+
+
+    //window.showPrompt(`Pos char:  ${position.character}, Pos: line ${position.line}, location: ${location} currentWord: ${currentWord}, location2: ${location2.character}, uri: ${uri}`);
+    const params = {
+      textDocument: {
+        uri: uri
+      },
+      location: location
+    };
+
+    client.sendRequest<any>('served/implementMethods', params).then((change: TextEdit[]) => {
+      if (!change.length){
+        //window.showPrompt('Nothing in the request');
+        window.showInformationMessage('failed to request client started: ' + client.started);
+      }
+      else{
+        //window.showMenuPicker(change.map(i => i.newText));
+        let startPos = change[0].range.start;
+        let start = document.getPosition(startPos.line, startPos.character);
+
+        change.forEach((c => {
+          c.newText = c.newText.replace(/(?:\${\d})/g, `throw new Exception("Not yet implemented")`);
+        }));
+
+
+        document.applyEdits(change);
+      }
+    });
+
+
+  });
+
+
+
+
+  const commandAddImport = commands.registerCommand("dlang.addImport", async () => {
+
+    const document = await workspace.document
+    const position = await window.getCursorPosition();
+    const range = document.getWordRangeAtPosition(position);
+    const cursorPos = await window.getCursorScreenPosition();
+    const location = document.getOffset(cursorPos.row, cursorPos.col);
+    let currentWord = document.textDocument.getText(range);
+    window.showPrompt(`currentCursorPos: ${position.line}, currentLocation: ${location}`);
+
+    if (range && range.start.character < position.character) {
+      const word = document.textDocument.getText(range);
+      currentWord = word;
+    }
+
+    const params = {
+      textDocument: {
+        uri: document.uri
+      },
+      name: 'loadSDL',
+      location: position.line
+    };
+
+    try {
+      client.sendRequest<ImportModification>('served/addImport', params).then((result: ImportModification) => {
+        if (result)
+        {
+          window.showMenuPicker(result.replacements.map(i=> i.content), 'add dependency').then(num => {
+            result.replacements[num].range;
+            window.moveTo({ line:  0, character:  0});
+          });
+        }
+        else
+          window.showPrompt('failed to request');
+
+      });
+
+    } catch (e) {
+      window.showPrompt('failed to request');
+      /* handle error */
+    }
+  });
+
+
+  context.subscriptions.push(commandlistArchTypes,
+    commandAddImport,
+    commandImplementMethods,
+    commandIgnoreDscannerKey,
+    commandListConfiguration,
+    services.registLanguageClient(client));
+
+  //await commands.executeCommand('dlang.dostuff');
 }
